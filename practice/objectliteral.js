8106a892a5a8b7c969dbdff3b0c94177ff6a895d
bogus <qazxwd44@hanmail.net>
2022-09-15 22:08:40 +0900

    allow

--- practice/objectliteral.js
+++ practice/objectliteral.js
@@ -13,14 +13,17 @@ oldObject.sayNode();
 oldObject.sayJS();
 console.log(oldObject.ES6);
 
+
+
 const newObject = {
     sayJS() {
         console.log('JS');
     },
 // sayNode처럼 속성명과 변수명이 동리한 경우네는 한 번만 써도 되게 바뀌었다.
     sayNode,
+// 하지만 ES2015 문법에서는  객체 리터런안에 동적 속성을 선언해도 된다.   
     [es +6 ]: 'Fanatatstic',
 };
 newObject.sayNode();
 newObject.sayJS();
-console.log(newObject.ES6);
+// console.log(newObject.ES6);
